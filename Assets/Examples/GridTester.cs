35d024f20d8c4c8437c261136ec535a75a767eda
Nakata Nobuyuki <nobu.nakata@gmail.com>
2015-11-19 18:01:03 +0900

    no message

---
+++ Assets/Examples/GridTester.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using NearestNeighborSearch;
+using System.Collections.Generic;
+
+public class GridTester : MonoBehaviour {
+	public HashGrid2D hashGrid;
+	public GameObject pointfab;
+	public Vector3 angularSpeed;
+	public float radius = 1f;
+	public int count = 1;
+
+	public enum DebugModeEnum { Normal, Distance, Nearest }
+	public float gizmoDistance = 1f;
+	public DebugModeEnum debugMode;
+
+	void Start () {
+		for (var i = 0; i < count; i++) {
+			var p = Instantiate(pointfab);
+			p.transform.SetParent(transform);
+			p.transform.localPosition = radius * Random.insideUnitSphere;
+			hashGrid.Add(p.transform);
+		}
+	}
+	void Update () {
+		transform.localRotation *= Quaternion.Euler(angularSpeed * Time.deltaTime);
+		hashGrid.Build();
+	}
+	void OnDrawGizmos() {
+		var d = gizmoDistance;
+
+		var count = hashGrid.PointCount();
+		for (var i = 0; i < count; i++) {
+			var t = hashGrid.GetTransform(i);
+			var p = hashGrid.GetPosition(i);
+			var neighbors = new LinkedList<HashGrid2D.Neighbor>();
+			var minSqrDist = float.MaxValue;
+			HashGrid2D.Neighbor nearest;
+			foreach (var n in hashGrid.Neighbors(p, d)) {
+				if (i < n.id) {
+					neighbors.AddLast(n);
+					if (n.sqrDistance < minSqrDist) {
+						minSqrDist = n.sqrDistance;
+						nearest = n;
+					}
+				}
+			}
+
+			Gizmos.color = Color.yellow;
+			switch (debugMode) {
+			default:
+				break;
+			case DebugModeEnum.Distance:
+				foreach (var n in neighbors)
+					Gizmos.DrawLine(t.position, n.point.position);
+				break;
+			case DebugModeEnum.Nearest:
+				if (neighbors.Count > 0)
+					Gizmos.DrawLine(t.position, nearest.point.position);
+				break;
+			}
+		}
+	}
+}
---
+++ Assets/Packages/NearestNeighborSearch/HashGrid2D.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace NearestNeighborSearch {
+
+	public class HashGrid2D : MonoBehaviour {
+		public int cellCount = 37;
+		public float cellSize = 1f;
+
+		int _pointCount;
+		float _gridSize;
+		List<Transform> _points;
+		List<Vector2> _positions;
+		List<int>[,] _cells;
+
+		void Awake() {
+			_pointCount = 0;
+			_points = new List<Transform>();
+			_positions = new List<Vector2>();
+		}
+
+		public void Add(Transform p) {
+			_pointCount++;
+			_points.Add(p);
+			_positions.Add(Vector2.zero);
+		}
+		public void Build() {
+			if (_cells == null || _cells.GetLength(0) != cellCount || _cells.GetLength(1) != cellCount)
+				InitGrid();
+			ClearGrid();
+			FillGrid();
+		}
+		public IEnumerable<Neighbor> Neighbors(Vector2 p, float distance) {
+			int x, y;
+			var limitSqrDistance = distance * distance;
+			Hash(p, out x, out y);
+			for (var dj = -1; dj <= 1; dj++) {
+				for (var di = -1; di <= 1; di++) {
+					var ix = Repeat(x + di);
+					var iy = Repeat(y + dj);
+					foreach (var id in _cells[ix, iy]) {
+						var q = _positions[id];
+						var sqrDist = (q - p).sqrMagnitude;
+						if (sqrDist < limitSqrDistance)
+							yield return new Neighbor(id, _points[id], _positions[id], sqrDist);
+					}
+				}
+			}
+		}
+		public int PointCount() { return _pointCount; }
+		public Transform GetTransform(int id) { return _points[id]; }
+		public Vector2 GetPosition(int id) { return _positions[id]; }
+
+		void InitGrid () {
+			_cells = new List<int>[cellCount, cellCount];
+			for (var j = 0; j < cellCount; j++)
+				for (var i = 0; i < cellCount; i++)
+					_cells [i, j] = new List<int> ();
+		}
+		void ClearGrid () {
+			for (var j = 0; j < cellCount; j++)
+				for (var i = 0; i < cellCount; i++)
+					_cells [i, j].Clear ();
+		}
+		void FillGrid() {
+			_gridSize = cellCount * cellSize;
+			for (var i = 0; i < _pointCount; i++) {
+				int x, y;
+				var pos = _positions [i] = (Vector2)_points [i].position;
+				Hash (pos, out x, out y);
+				_cells [x, y].Add(i);
+			}
+		}
+		void Hash(Vector2 pos, out int ix, out int iy) {
+			ix = (int)((pos.x - Mathf.FloorToInt(pos.x / _gridSize) * _gridSize) / cellSize);
+			iy = (int)((pos.y - Mathf.FloorToInt(pos.y / _gridSize) * _gridSize) / cellSize);
+
+			ix = Repeat(ix);
+			iy = Repeat(iy);
+		}
+		int Repeat(int x) {
+			while (x < 0)
+				x += cellCount;
+			while (x >= cellCount)
+				x -= cellCount;
+			return x;
+		}
+
+		public struct Neighbor {
+			public readonly int id;
+			public readonly float sqrDistance;
+			public readonly Transform point;
+			public readonly Vector2 position;
+
+			public Neighbor(int id, Transform point, Vector2 position, float sqrDistance) {
+				this.id = id;
+				this.point = point;
+				this.position = position;
+				this.sqrDistance = sqrDistance;
+			}
+		}
+	}
+}
